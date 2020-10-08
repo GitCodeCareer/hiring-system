5abe0d762fbf8b463e85112f9110c792208ad5e4
Diego Giuliani <giuliani.diego@gmail.com>
2020-10-07 21:07:45 -0300

    Adjust main navigation

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom';
 import JobsPage from './pages/jobs.page';
 import LoginPage from './pages/login.page';
-import RegisterPage from './pages/register.page'
+import RegisterPage from './pages/register.page';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/global.css';
 
 render(
   <Router>
-    <Route path="/" component={LoginPage} exact/>
-    <Route path="/register" component={RegisterPage} exact/>
+    <Route path="/login" component={LoginPage} exact />
+    <Route path="/register" component={RegisterPage} exact />
     <Route path="/jobs" component={JobsPage} exact />
+    <Route path="/" component={JobsPage} exact />
   </Router>,
   document.getElementById('root-app')
-)
+);
