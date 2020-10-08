11607ac80c5e9fe0bef0a7d530470a8a4f53a96b
Diego Giuliani <giuliani.diego@gmail.com>
2020-10-07 21:11:08 -0300

    Refactor jobs page

--- src/frontend/pages/jobs.page.js
+++ src/frontend/pages/jobs.page.js
@@ -1,121 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import {
-  Button,
-  Container,
-  ListGroup,
-  ListGroupItem,
-  Input
-} from 'reactstrap';
+import { Button, Container, ListGroup, ListGroupItem, Input } from 'reactstrap';
 import Navbar from '../components/navbar.component';
-
-const listOfJob = [
-  {
-    id: '1',
-    title: 'Senior backend developer',
-    company: 'Google.inc',
-    location: 'Addison',
-    jobType: "Full-time",
-    date: '20, Sept 2020',
-  },
-  {
-    id: '2',
-    title: 'Senior backend developer',
-    company: 'Google.inc',
-    location: 'Berkeley County',
-    jobType: "Full-time",
-    date: '20, Sept 2020',
-  },
-  {
-    id: '3',
-    title: 'Senior backend developer',
-    company: 'Google.inc',
-    location: 'Cambridge',
-    jobType: "Full-time",
-    date: '20, Sept 2020',
-  },
-  {
-    id: '4',
-    title: 'Senior backend developer',
-    company: 'Google.inc',
-    location: 'Chandler',
-    jobType: "Full-time",
-    date: '20, Sept 2020',
-  },
-  {
-    id: '5',
-    title: 'Senior backend developer',
-    company: 'Google.inc',
-    location: 'Clarksville',
-    jobType: "Full-time",
-    date: '20, Sept 2020',
-  },
-  {
-    id: '6',
-    title: 'Senior backend developer',
-    company: 'Google.inc',
-    location: 'Detroit',
-    jobType: "Full-time",
-    date: '20, Sept 2020',
-  },
-  {
-    id: '7',
-    title: 'Senior backend developer',
-    company: 'Google.inc',
-    location: 'Dallas',
-    jobType: "Full-time",
-    date: '20, Sept 2020',
-  },
-  {
-    id: '8',
-    title: 'Senior backend developer',
-    company: 'Facebook',
-    location: 'Salt Lake City',
-    jobType: "Full-time",
-    date: '20, Sept 2020',
-  },
-  {
-    id: '9',
-    title: 'Senior backend developer',
-    company: 'Netflix',
-    location: 'Sunnyvale',
-    jobType: "Full-time",
-    date: '20, Sept 2020',
-  },
-  {
-    id: '10',
-    title: 'Senior backend developer',
-    company: 'Amazon',
-    location: 'Washington D.C.',
-    jobType: "Full-time",
-    date: '20, Sept 2020',
-  }
-]
+import { listOfJob } from '../data/jobs';
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jobs: listOfJob
-    }
+      jobs: listOfJob,
+    };
     this.handleChange = this.handleChange.bind(this);
     this.filterWithPropery = this.filterWithPropery.bind(this);
   }
 
   filterWithPropery(properyName, value) {
     let filteredJobs = this.state.jobs.filter((item) => {
-      return item[properyName].toLowerCase().includes(value.toLowerCase())
-    })
-    this.setState({ jobs: filteredJobs })
+      return item[properyName].toLowerCase().includes(value.toLowerCase());
+    });
+    this.setState({ jobs: filteredJobs });
   }
 
   handleChange = (e) => {
-    if(e.target.value.length >= 3) {
-      this.filterWithPropery(e.target.name, e.target.value)
-    }else {
-      this.setState({ jobs: listOfJob })
+    if (e.target.value.length >= 3) {
+      this.filterWithPropery(e.target.name, e.target.value);
+    } else {
+      this.setState({ jobs: listOfJob });
     }
-  }
+  };
 
   render = () => {
     const { jobs } = this.state;
@@ -123,7 +35,7 @@ class HomePage extends Component {
       <div>
         <Navbar />
         <Container className="mt-4 mb-4">
-          <h1>List of aviable jobs</h1>
+          <h1>List of available jobs</h1>
           <div className="d-flex justify-content-end mt-3">
             <div className="w-25 mr-2">
               <Input
@@ -145,27 +57,27 @@ class HomePage extends Component {
           )}
           <ListGroup className="mt-3">
             {jobs.map((item, index) => (
-              <ListGroupItem disabled tag="a" href="#">
+              <ListGroupItem key={item.id} disabled tag="a" href="#">
                 <div className="d-flex flex-row">
                   <div className="mr-auto">
                     <h4 className="text-dark">{item.title}</h4>
                   </div>
-                  <div>
-                    {item.location}
-                  </div>
+                  <div>{item.location}</div>
                   <div className="ml-auto">
                     <Button color="primary">See details</Button>
                   </div>
                 </div>
                 <span className="text-muted">{item.company}</span> <br />
-                <span className="text-tiny">{item.date} - {item.jobType}</span> 
+                <span className="text-tiny">
+                  {item.date} - {item.jobType}
+                </span>
               </ListGroupItem>
             ))}
           </ListGroup>
         </Container>
       </div>
-    )
-  }
+    );
+  };
 }
 
 export default HomePage;
