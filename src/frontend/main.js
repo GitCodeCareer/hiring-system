e2d595f26471270ac16b3e95144f9e34f077821f
Bresia Prudente <bprudew@gmail.com>
2020-10-09 15:53:44 -0500

    Added apply page, form validation, and file input. - Added apply page - Added form validation per Bootstrap's documentation. Also added specific validation for email and phone numbers. - Added file input

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -3,7 +3,8 @@ import { render } from 'react-dom';
 import { HashRouter as Router, Route } from 'react-router-dom'
 import JobsPage from './pages/jobs.page';
 import LoginPage from './pages/login.page';
-import RegisterPage from './pages/register.page'
+import RegisterPage from './pages/register.page';
+import ApplyPage from './pages/apply.page';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/global.css';
 
@@ -12,6 +13,7 @@ render(
     <Route path="/" component={LoginPage} exact/>
     <Route path="/register" component={RegisterPage} exact/>
     <Route path="/jobs" component={JobsPage} exact />
+    <Route path="/apply" component={ApplyPage} />
   </Router>,
   document.getElementById('root-app')
 )
