9b382a0d7f8bd41b9e420882cfdb829af398b5c4
Rushikesh <rushikeshsunilganesh@gmail.com>
2020-10-29 09:05:06 +0530

    changes in all job view

--- src/frontend/pages/jobs.page.js
+++ src/frontend/pages/jobs.page.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Container, ListGroup, ListGroupItem, Input } from 'reactstrap';
+import { Button, Container, ListGroup, ListGroupItem, Input} from 'reactstrap';
 import Navbar from '../components/navbar.component';
 import { listOfJob } from '../data/jobs';
 
@@ -9,10 +9,16 @@ class HomePage extends Component {
     super(props);
     this.state = {
       jobs: listOfJob,
+      isOpen: false
     };
+    this.toggle = this.toggle.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.filterWithPropery = this.filterWithPropery.bind(this);
   }
+
+  toggle = () => {
+    this.setState({isOpen: !this.state.isOpen})
+}
 
   filterWithPropery(properyName, value) {
     let filteredJobs = this.state.jobs.filter((item) => {
@@ -36,21 +42,24 @@ class HomePage extends Component {
         <Navbar />
         <Container className="mt-4 mb-4">
           <h1>List of available jobs</h1>
-          <div className="d-flex justify-content-end mt-3">
-            <div className="w-25 mr-2">
-              <Input
-                name="company"
-                onChange={this.handleChange}
-                placeholder="Search with company name"
-              />
-            </div>
-            <div className="w-25">
-              <Input
-                name="location"
-                onChange={this.handleChange}
-                placeholder="Search with location"
-              />
-            </div>
+          <div className="d-flex justify-content-center mt-3">
+                    <div className="input-group  ">
+                      <Input
+                      className="inputsearch "
+                      name="company"
+                      onChange={this.handleChange}
+                      placeholder="Search with company name"
+                      />
+
+                      <Input
+                       className="inputsearch "
+                        name="location"
+                        onChange={this.handleChange}
+                        placeholder="Search with location"
+                      />
+                   </div>
+            
+     
           </div>
           {jobs.length == 0 && (
             <h3 className="text-center mt-5">No result found</h3>
@@ -64,7 +73,9 @@ class HomePage extends Component {
                   </div>
                   <div>{item.location}</div>
                   <div className="ml-auto">
-                    <Button color="primary">See details</Button>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-three-dots-vertical" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                    </svg>
                   </div>
                 </div>
                 <span className="text-muted">{item.company}</span> <br />
