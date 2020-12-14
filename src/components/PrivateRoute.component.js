9dd2e7f4625e794935aa31f0fb3c94fa5f2d1617
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-12-14 16:56:27 +0000

    refactor: PrivateRoute
    removed console log and unused dependencies

--- src/components/PrivateRoute.component.js
+++ src/components/PrivateRoute.component.js
@@ -2,17 +2,9 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 const PrivateRouteComponent = ({ children, ...rest }) => {
-  const {
-    isLoading,
-
-    isAuthenticated,
-    error,
-    user,
-    loginWithRedirect,
-    logout,
-  } = useAuth0();
+  const { isAuthenticated } = useAuth0();
   const isUser = isAuthenticated;
-  console.log(isUser);
+
   return (
     <Route
       {...rest}
