299e31c915c16a10750aa9229bb9eece38a54c72
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-12-14 16:41:23 +0000

    refactor: Improved index.js removed unused dependencies
    removed login and registation as useAuth0 provides a view

--- src/index.js
+++ src/index.js
@@ -1,31 +1,30 @@
 import React from "react";
-// import { render } from "react-dom";
 import ReactDOM from "react-dom";
-
 import { HashRouter as Router, Route } from "react-router-dom";
 
 import { Auth0Provider } from "@auth0/auth0-react";
 
 import JobsPage from "./pages/jobs.page";
-import LoginPage from "./pages/login.page";
-import RegisterPage from "./pages/register.page";
+// import LoginPage from "./pages/login.page";
+// import RegisterPage from "./pages/register.page";
 import ApplyPage from "./pages/apply.page";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles/global.css";
 import AdminPanel from "./pages/admin.page";
 import ApplicantsPage from "./pages/applicants.page";
 import CreateJob from "./pages/createJob.page";
 import EditJob from "./pages/editJob.page";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/global.css";
+
 import PrivateRouteComponent from "./components/PrivateRoute.component";
 //Add the below component for routes which are to be kept private
-import PrivateRoute from "./routing/PrivateRoute";
+// import PrivateRoute from "./routing/PrivateRoute";
 
 const App = () => {
   return (
     <Router>
-      <Route path="/login" component={LoginPage} exact />
-      <Route path="/register" component={RegisterPage} exact />
+      {/* <Route path="/login" component={LoginPage} exact />
+      <Route path="/register" component={RegisterPage} exact /> */}
       <Route path="/jobs" component={JobsPage} exact />
       <Route path="/apply" component={ApplyPage} />
       <Route path="/" component={JobsPage} exact />
@@ -37,9 +36,11 @@ const App = () => {
       <PrivateRouteComponent path="hire/applicants" exact>
         <ApplicantsPage />
       </PrivateRouteComponent>
+
       <PrivateRouteComponent path="hire/new" exact>
         <CreateJob />
       </PrivateRouteComponent>
+
       <PrivateRouteComponent path="hire/:jobid" exact>
         <EditJob />
       </PrivateRouteComponent>
