a55ece498f1500b739533c81db7ca59a854c4d34
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-01-04 10:50:12 +0000

    refactor: Converted jobpage container from class component to functional component

--- src/pages/jobs.page.js
+++ src/pages/jobs.page.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, ListGroup, Input } from "reactstrap";
 import Navbar from "../components/navbar.component";
 
@@ -6,69 +6,63 @@ import JobCard from "../components/Job/Job.component";
 
 import { listOfJob } from "../data/jobs";
 
-class HomePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      jobs: listOfJob,
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.filterWithPropery = this.filterWithPropery.bind(this);
-  }
+const HomePage = () => {
+  const [jobs, setJobs] = useState([]);
 
-  filterWithPropery(properyName, value) {
-    let filteredJobs = this.state.jobs.filter((item) => {
-      console.log(item);
+  const filterWithPropery = (properyName, value) => {
+    let filteredJobs = jobs.filter((item) => {
       return item[properyName].toLowerCase().includes(value.toLowerCase());
     });
-    this.setState({ jobs: filteredJobs });
-  }
+    setJobs(filteredJobs);
+  };
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     if (e.target.value.length >= 3) {
-      this.filterWithPropery(e.target.name, e.target.value);
+      filterWithPropery(e.target.name, e.target.value);
     } else {
-      this.setState({ jobs: listOfJob });
+      // need to put old jobs into local storage then retrieve after filter canceled
+      setJobs(listOfJob);
     }
   };
 
-  render = () => {
-    const { jobs } = this.state;
-    return (
-      <div>
-        <Navbar />
-        <Container className="mt-4 mb-4">
-          <h1>List of available jobs</h1>
-          <div className="d-flex justify-content-end mt-3">
-            <div className="w-25 mr-2">
-              <Input
-                name="company"
-                onChange={this.handleChange}
-                placeholder="Search with company name"
-              />
-            </div>
-            <div className="w-25">
-              <Input
-                name="location"
-                onChange={this.handleChange}
-                placeholder="Search with location"
-              />
-            </div>
+  useEffect(() => {
+    setJobs(listOfJob);
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <Container className="mt-4 mb-4">
+        <h1>List of available jobs</h1>
+        <div className="d-flex justify-content-end mt-3">
+          <div className="w-25 mr-2">
+            <Input
+              name="jobCreator"
+              onChange={handleChange}
+              placeholder="Search with company name"
+            />
           </div>
+          <div className="w-25">
+            <Input
+              name="jobLocation"
+              onChange={handleChange}
+              placeholder="Search with location"
+            />
+          </div>
+        </div>
 
-          {jobs.length === 0 && (
-            <h3 className="text-center mt-5">No result found</h3>
-          )}
+        {jobs.length === 0 && (
+          <h3 className="text-center mt-5">No result found</h3>
+        )}
 
-          <ListGroup className="mt-3">
-            {jobs.map((job) => (
-              <JobCard job={job} />
-            ))}
-          </ListGroup>
-        </Container>
-      </div>
-    );
-  };
-}
+        <ListGroup className="mt-3">
+          {jobs.map((job) => (
+            <JobCard key={job.userId} job={job} />
+          ))}
+        </ListGroup>
+      </Container>
+    </div>
+  );
+};
 
 export default HomePage;
