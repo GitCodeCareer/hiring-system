f9c2a77eb7a74624dbb16ddce2aa1251ed1dbd6b
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-01-04 12:49:22 +0000

    enhancement: Cleaned up the design and added font awesome icons
    Added location icon and ellipisis to use as a dropdown later

--- src/components/Job/Job.component.js
+++ src/components/Job/Job.component.js
@@ -1,7 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Badge, Button, ListGroupItem } from "reactstrap";
-
+import { Badge, ListGroupItem } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisV, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 import "../Job/job.styles.css";
 
 const Job = ({ job }) => {
@@ -20,25 +21,31 @@ const Job = ({ job }) => {
   return (
     <ListGroupItem className="job-card" tag="a" href={jobApplyURL} key={userId}>
       <div className="d-flex flex-column">
-        <div className="mb-3">
+        <div className="job-card--header mb-3">
           <img className="job-image" src={jobCreatorLogo} alt={jobCreator} />
           <span className="text-tiny">{dateModified}</span>
-          <span>...</span>
+          <span className="drop-down-icon">
+            <FontAwesomeIcon icon={faEllipsisV} />
+          </span>
         </div>
+
         <div className="mr-auto">
           <h4 className="job-card--title text-dark">{jobTitle}</h4>
-          <h6 className="text-muted">{jobLocation}</h6>
+          <h6 className="text-muted">
+            <FontAwesomeIcon icon={faMapMarkerAlt} />
+            <span className="ml-2">{jobLocation}</span>
+          </h6>
 
           <p>
             {jobDescription.replace(/(<([^>]+)>)/gi, "").substring(0, 100)} ...
           </p>
+
           <Badge className="p-2 mt-1 mr-2" color="primary">
             {jobWorkType}
           </Badge>
         </div>
       </div>
 
-      {/* <span className="text-muted">{jobCreator}</span> */}
       <br />
     </ListGroupItem>
   );
