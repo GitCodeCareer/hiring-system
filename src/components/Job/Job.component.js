51fae3f352442b22a105f157d0be9f148d120d34
Daniel Philip Johnson <61696382+danielphilipjohnson@users.noreply.github.com>
2021-08-11 02:44:35 +0100

    Enhancement  jobcard (#52)
    * enhancement: Updated local data to include new fields, extracted job component from jobs page.
    
    The local job data was updated to include new fields for images, jobtype and job link
    
    Jobs page is a container and contained too much logic so it was split to a seperate dumb functional component
    
    * refactor: Cleaned unused code and removed old dependencies from job page
    
    * refactor: Converted jobpage container from class component to functional component
    
    * enhancement: Styled basic job card
    
    * enhancement: Cleaned up the design and added font awesome icons
    
    Added location icon and ellipisis to use as a dropdown later
    
    * new dependency: Used moment.js to work with dates
    
    * styled: layout of date
    
    * style: When a job doesnt have an image a header containing company name is provided
    
    * enhancement: Added dropdown so a user can save job or remove it
    
    * style: Made job card responsive on iphone5 and tested responsive for deviced with bigger screen width
    
    * refactor: optimised styles and ordered in sequence of layout
    
    * refactor: organised Job component and cleaned it
    
    * refactor: deleted duplicated code for job creator logo
    
    If an image exists only draw the image otherwise display a h3 that displays company name

---
+++ src/components/Job/Job.component.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+
+import PropTypes from "prop-types";
+
+import moment from "moment";
+
+import {
+  Badge,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  ListGroupItem,
+} from "reactstrap";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBan,
+  faEllipsisV,
+  faMapMarkerAlt,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+
+import "../Job/job.styles.css";
+
+const Job = ({ job }) => {
+  const {
+    userId,
+    jobTitle,
+    jobCreatorLogo,
+    jobLocation,
+    // jobApplyURL,
+    jobCreator,
+    dateModified,
+    jobDescription,
+    jobWorkType,
+  } = job;
+
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+
+  return (
+    <ListGroupItem className="job-card" key={userId}>
+      <div className="d-flex flex-column">
+        <div className="job-card--header mb-3">
+          {jobCreatorLogo ? (
+            <img className="job-image" src={jobCreatorLogo} alt={jobCreator} />
+          ) : (
+            <h3 className="company mr-4">{jobCreator}</h3>
+          )}
+
+          <span className="text-tiny job-time">
+            {moment(dateModified).fromNow()}
+          </span>
+
+          <Dropdown
+            className="drop-down-icon"
+            isOpen={dropdownOpen}
+            toggle={toggle}
+          >
+            <DropdownToggle>
+              <FontAwesomeIcon icon={faEllipsisV} />
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem>
+                <FontAwesomeIcon icon={faHeart} className="mr-1" /> Save Job
+              </DropdownItem>
+              <DropdownItem>
+                <FontAwesomeIcon icon={faBan} className="mr-2" />
+                Not Interested
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </div>
+
+        <div className="mr-auto">
+          <h4 className="job-card--title text-dark">{jobTitle}</h4>
+          <h6 className="text-muted">
+            <FontAwesomeIcon icon={faMapMarkerAlt} />
+            <span className="ml-2">{jobLocation}</span>
+          </h6>
+
+          <p>
+            {jobDescription.replace(/(<([^>]+)>)/gi, "").substring(0, 100)} ...
+          </p>
+
+          <Badge className="p-2 mt-1 mr-2" color="primary">
+            {jobWorkType}
+          </Badge>
+        </div>
+      </div>
+    </ListGroupItem>
+  );
+};
+
+Job.propTypes = {
+  job: PropTypes.object.isRequired,
+};
+
+export default Job;
